218015c3c1cbbfb4c96e304a0acca66d28d08e8c
brustolin23 <102733510+brustolin23@users.noreply.github.com>
2024-06-24 15:25:07 -0300

    Atualização da função e dos testes

--- PytestPTI/codigo/game.py
+++ PytestPTI/codigo/game.py
@@ -1,2 +1,8 @@
 def jogo(numb):
-    return 1
+    if (numb % 3 == 0) & (numb % 5 == 0):
+        return 'romeu e julieta'
+    if numb % 3 == 0:
+        return 'queijo'
+    if numb % 5 == 0:
+        return 'goiabada'
+    return numb
--- PytestPTI/tests/testeDoPytest.py
+++ PytestPTI/tests/testeDoPytest.py
@@ -11,3 +11,21 @@ def test_quando_jogo_receber_2_entao_retorna_2():
     esperado = 2
     resultado = jogo(entrada)
     assert resultado == esperado
+
+def test_quando_jogo_receber_3_entao_retorna_queijo():
+    entrada = 3
+    esperado = 'queijo'
+    resultado = jogo(entrada)
+    assert resultado == esperado
+
+def test_quando_jogo_receber_5_entao_retorna_goiabada():
+    entrada = 5
+    esperado = 'goiabada'
+    resultado = jogo(entrada)
+    assert resultado == esperado
+
+def test_quando_jogo_receber_15_entao_retorna_goiabada():
+    entrada = 15
+    esperado = 'romeu e julieta'
+    resultado = jogo(entrada)
+    assert resultado == esperado
